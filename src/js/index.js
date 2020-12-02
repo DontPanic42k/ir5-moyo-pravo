ac697e8a0c72ba11a1005dafbaf6b87e42a114c9
joe_yes <artem@yes-idea.ru>
2020-12-03 01:04:40 +0500

    update

--- src/js/index.js
+++ src/js/index.js
@@ -18,3 +18,10 @@ import '../js/tabs'
 
 import '../js/forms'
 import '../js/swiper-scripts'
+
+
+// при клике на модалку - меняем ее тайтл на название кнопки
+$('.btn.btn-orange.popupActivation').on('click', function(){
+    let title = $(this).html();
+    $('.form.form-popup-js .form__title .title').html( title );
+});
--- src/js/swiper-scripts.js
+++ src/js/swiper-scripts.js
@@ -1,82 +1,199 @@
-// $(window).on('resize', function(){
-//     if( $(window).width() > 768 ){
-        let marginLeft = ( $(window).width() - $('.page-bankruptcy-our-team .container').width() ) / 2;
-        let containerWidth = $('.page-bankruptcy-our-team .container').width();
-        
-        $('.page-bankruptcy-our-team .swiper-container').css({
-            'width' : containerWidth + marginLeft + 'px',
-        });
-        
-        $('.page-bankruptcy-our-team-list').css({
-            // 'margin-left' : marginLeft + 'px',
-            'overflow' : 'hidden',
-            'width' : containerWidth + marginLeft + 'px',
-        });
-//     }
-
-// });
-
-
-
 import Swiper from 'swiper'
 import 'swiper/swiper-bundle.css'
 
-let swiper_el = '.sw-1';
 
-let swiper = new Swiper(swiper_el, {
-    slidesPerView: 'auto',
-    spaceBetween: 20,
-    navigation: {
-        prevEl: '.mleft',
-        nextEl: '.mright',
-    },
-    loop: true,
-    autoplay: {
-        delay: 2000,
-    },
-    mousewheel: true,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        //type: 'bullets',
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-        2000 : { 
-           slidesPerView: 'auto',
-        },
-        769: {
-            slidesPerView: 'auto',
-        },
-        1: {
-           slidesPerView: 1,
-            // spaceBetween: 20
-        },
-    },
-    on: {
-        init: function(){
-            // полное количество
-            let totalSlides = $( swiper_el ).find('.swiper-slide:not(.swiper-slide-duplicate)').length;
-            $( '.sw-1-nav' ).find('.slider-nav-count span[data-slide_count="total"]').html( totalSlides );
 
-            let slide_active_num = $( swiper_el ).find('.swiper-slide.swiper-slide-active').attr('data-swiper-slide-index') * 1  +  1;
-            $( '.sw-1-nav' ).find('.slider-nav-count span[data-slide_count="current"]').html( slide_active_num );
+let swiper_our_team = () => {
+    // $(window).on('resize', function(){
+    //     if( $(window).width() > 768 ){
+            let marginLeft = ( $(window).width() - $('.page-bankruptcy-our-team .container').width() ) / 2;
+            let containerWidth = $('.page-bankruptcy-our-team .container').width();
+            
+            $('.page-bankruptcy-our-team .swiper-container').css({
+                'width' : containerWidth + marginLeft + 'px',
+            });
+            
+            $('.page-bankruptcy-our-team-list').css({
+                // 'margin-left' : marginLeft + 'px',
+                'overflow' : 'hidden',
+                'width' : containerWidth + marginLeft + 'px',
+            });
+    //     }
+    // });
+
+
+    let swiper_el = '.sw-1';
+
+    let swiper = new Swiper(swiper_el, {
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+        navigation: {
+            prevEl: '.mleft',
+            nextEl: '.mright',
         },
-        slideChangeTransitionEnd: function(){
-            let slide_active_num = $( swiper_el ).find('.swiper-slide.swiper-slide-active').attr('data-swiper-slide-index') * 1  +  1;
-            $( '.sw-1-nav' ).find('.slider-nav-count span[data-slide_count="current"]').html( slide_active_num );
-            // console.log(slide_active_num);
-        }
-    },
-});
+        loop: true,
+        autoplay: {
+            delay: 2000,
+        },
+        mousewheel: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            //type: 'bullets',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            2000 : { 
+                slidesPerView: 'auto',
+            },
+            769: {
+                slidesPerView: 'auto',
+            },
+            1: {
+            slidesPerView: 1,
+                // spaceBetween: 20
+            },
+        },
+        on: {
+            init: function(){
+                // полное количество
+                let totalSlides = $( swiper_el ).find('.swiper-slide:not(.swiper-slide-duplicate)').length;
+                $( '.sw-1-nav' ).find('.slider-nav-count span[data-slide_count="total"]').html( totalSlides );
 
-document.querySelector('.page-bankruptcy-our-team-list .swiper-button-next').addEventListener('click', () => {
-    swiper.slideNext();
-});
+                let slide_active_num = $( swiper_el ).find('.swiper-slide.swiper-slide-active').attr('data-swiper-slide-index') * 1  +  1;
+                $( '.sw-1-nav' ).find('.slider-nav-count span[data-slide_count="current"]').html( slide_active_num );
+            },
+            slideChangeTransitionEnd: function(){
+                let slide_active_num = $( swiper_el ).find('.swiper-slide.swiper-slide-active').attr('data-swiper-slide-index') * 1  +  1;
+                $( '.sw-1-nav' ).find('.slider-nav-count span[data-slide_count="current"]').html( slide_active_num );
+                // console.log(slide_active_num);
+            }
+        },
+    });
 
-document.querySelector('.page-bankruptcy-our-team-list .swiper-button-prev').addEventListener('click', () => {
-    swiper.slidePrev();
-});
+    document.querySelector('.page-bankruptcy-our-team-list .swiper-button-next').addEventListener('click', () => {
+        swiper.slideNext();
+    });
+
+    document.querySelector('.page-bankruptcy-our-team-list .swiper-button-prev').addEventListener('click', () => {
+        swiper.slidePrev();
+    });
+
+}
+
+
+
+
+
+let swiper_results_of_work = () => {
+    // $(window).on('resize', function(){
+    //     if( $(window).width() > 768 ){
+            // let marginLeft = ( $(window).width() - $('.page-bankruptcy-our-team .container').width() ) / 2;
+            // let containerWidth = $('.page-bankruptcy-our-team .container').width();
+            
+            // $('.page-bankruptcy-our-team .swiper-container').css({
+            //     'width' : containerWidth + marginLeft + 'px',
+            // });
+            
+            // $('.page-bankruptcy-our-team-list').css({
+            //     // 'margin-left' : marginLeft + 'px',
+            //     'overflow' : 'hidden',
+            //     'width' : containerWidth + marginLeft + 'px',
+            // });
+    //     }
+    // });
+
+
+    let swiper_el = '.sw-2';
+
+    let swiper = new Swiper(swiper_el, {
+        // slidesPerView: 'auto',
+        slidesPerView: 4,
+        spaceBetween: 39,
+        navigation: {
+            prevEl: '.mleft',
+            nextEl: '.mright',
+        },
+        loop: true,
+        autoplay: {
+            delay: 2000,
+        },
+        mousewheel: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            type: 'bullets',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            // 2000 : { 
+            //     slidesPerView: 'auto',
+            // },
+            768: {
+                slidesPerView: 4,
+            },
+            660: {
+                slidesPerView: 3,
+            },
+            375: {
+                slidesPerView: 2,
+            },
+            1: {
+            slidesPerView: 1,
+                // spaceBetween: 20
+            },
+        },
+        // on: {
+        //     init: function(){
+        //         // полное количество
+        //         let totalSlides = $( swiper_el ).find('.swiper-slide:not(.swiper-slide-duplicate)').length;
+        //         $( '.sw-1-nav' ).find('.slider-nav-count span[data-slide_count="total"]').html( totalSlides );
+
+        //         let slide_active_num = $( swiper_el ).find('.swiper-slide.swiper-slide-active').attr('data-swiper-slide-index') * 1  +  1;
+        //         $( '.sw-1-nav' ).find('.slider-nav-count span[data-slide_count="current"]').html( slide_active_num );
+        //     },
+        //     slideChangeTransitionEnd: function(){
+        //         let slide_active_num = $( swiper_el ).find('.swiper-slide.swiper-slide-active').attr('data-swiper-slide-index') * 1  +  1;
+        //         $( '.sw-1-nav' ).find('.slider-nav-count span[data-slide_count="current"]').html( slide_active_num );
+        //     }
+        // },
+    });
+
+    document.querySelector('.page-bankruptcy-results-of-work-docs-slider .swiper-button-next').addEventListener('click', () => {
+        swiper.slideNext();
+    });
+
+    document.querySelector('.page-bankruptcy-results-of-work-docs-slider .swiper-button-prev').addEventListener('click', () => {
+        swiper.slidePrev();
+    });
+
+}
+
+
+
+
+swiper_our_team();
+swiper_results_of_work();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
